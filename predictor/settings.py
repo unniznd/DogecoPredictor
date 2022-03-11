0650e90977a5151e6b91ac852f47cf81256aaad5
unniznd <au6695@gmail.com>
2022-03-11 07:23:51 +0530

    settings updated

--- predictor/settings.py
+++ predictor/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1qyfupg7u78g*lyc+s97v(y(f5xhaes4e(ix91ded70n*hpj+u"
-#os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dogecopredictor.herokuapp.com','*']
+ALLOWED_HOSTS = ['dogecopredictor.herokuapp.com']
 
 
 # Application definition
